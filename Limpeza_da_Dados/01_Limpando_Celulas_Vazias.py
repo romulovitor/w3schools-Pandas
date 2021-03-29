72838378860146433b540ed57a603bc44e5ecd43
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-29 10:47:00 -0300

    limpeza de célular vazias

---
+++ Limpeza_da_Dados/01_Limpando_Celulas_Vazias.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+# Retorne um novo quadro de dados sem células vazias:
+new_df = df.dropna()
+
+print(new_df.to_string(), '\n')
+
+
+# Remova todas as linhas com valores NULL:
+df = pd.read_csv('data.csv')
+
+df.dropna(inplace=True)
+
+print(df.to_string(), '\n')
+
+
+# Substituir Valores Vazios
+df = pd.read_csv('data.csv')
+
+df.fillna('VALOR_NULL', inplace=True)
+print(df.to_string(), '\n')
+
+# Substituir apenas por colunas especificadas
+df = pd.read_csv('data.csv')
+
+df['idade'].fillna(0, inplace=True)
+
+print(df.to_string(), '\n')
+
+
+# Substitua usando média, mediana ou modo
+df = pd.read_csv('data.csv')
+
+media = df["idade"].mean()
+
+df['idade'].fillna(media, inplace=True)
+print(df.to_string(), '\m')
+
+
+# Calcule o MODO e substitua quaisquer valores vazios por ele:
+# Modo = o valor que aparece com mais frequência.
+df = pd.read_csv('data.csv')
+
+x = df["Calories"].mode()[0]
+
+df["Calories"].fillna(x, inplace = True)
